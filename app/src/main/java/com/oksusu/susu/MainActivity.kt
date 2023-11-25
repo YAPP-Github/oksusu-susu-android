05ff10781b56afee77e1fd26b19728588e61d01e
jinukeu <koownij@kakao.com>
2023-11-26 00:18:18 +0900

    chore: 오타 제거

--- app/src/main/java/com/oksusu/susu/MainActivity.kt
+++ app/src/main/java/com/oksusu/susu/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SusuThemwe {
+            SusuTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
