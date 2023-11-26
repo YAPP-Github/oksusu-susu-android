ae2504cb67d81c9eef3ee3724f35f7ef5ac2bf71
jinukeu <koownij@kakao.com>
2023-11-26 20:59:34 +0900

    chore: local 모듈 생성

---
+++ local/build.gradle.kts
@@ -0,0 +1,23 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+  id("susu.android.library")
+  id("susu.android.hilt")
+  alias(libs.plugins.ksp)
+}
+
+android {
+  namespace = "com.susu.local"
+}
+
+dependencies {
+  implementation(projects.core.model)
+  implementation(projects.core.database)
+
+  ksp(libs.room.compiler)
+  implementation(libs.room.runtime)
+  implementation(libs.room.ktx)
+
+  implementation(libs.bundles.coroutine)
+  implementation(libs.androidx.datastore.core)
+  implementation(libs.androidx.datastore.preferences)
+}
