d6fa988ba98cbbc58cdc53757ebed1b3a5249276
jinukeu <koownij@kakao.com>
2023-11-26 20:37:27 +0900

    chore: :core:database 모듈 추가

---
+++ core/database/build.gradle.kts
@@ -0,0 +1,37 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+  id("susu.android.library")
+  id("susu.android.hilt")
+  alias(libs.plugins.ksp)
+  alias(libs.plugins.protobuf)
+}
+
+android {
+    namespace = "com.susu.core.database"
+}
+
+protobuf {
+  protoc {
+    artifact = libs.protobuf.protoc.get().toString()
+  }
+  generateProtoTasks {
+    all().forEach { task ->
+      task.builtins {
+        register("java") {
+          option("lite")
+        }
+      }
+    }
+  }
+}
+
+dependencies {
+  ksp(libs.room.compiler)
+  implementation(libs.room.runtime)
+  implementation(libs.room.ktx)
+
+  implementation(libs.bundles.coroutine)
+  implementation(libs.androidx.datastore.core)
+  implementation(libs.androidx.datastore.preferences)
+  implementation(libs.protobuf.kotlin.lite)
+}
