9505d1911250676cd7c50dca2b5b15f2ae79c029
yangsooplus <yangsooplus@naver.com>
2023-12-09 15:45:55 +0900

    feat: Hilt Network Module 추가

---
+++ data/src/main/java/com/susu/data/di/NetworkModule.kt
@@ -0,0 +1,57 @@
+package com.susu.data.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.susu.data.network.TokenAuthenticator
+import com.susu.data.network.TokenInterceptor
+import com.susu.domain.repository.TokenRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = ""
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        tokenInterceptor: TokenInterceptor,
+        tokenAuthenticator: TokenAuthenticator,
+    ): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient.Builder()
+            .addInterceptor(tokenInterceptor)
+            .addInterceptor(loggingInterceptor)
+            .authenticator(tokenAuthenticator)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideTokenInterceptor(tokenRepository: TokenRepository): TokenInterceptor =
+        TokenInterceptor(tokenRepository)
+
+    @Singleton
+    @Provides
+    fun provideTokenAuthenticator(tokenRepository: TokenRepository): TokenAuthenticator =
+        TokenAuthenticator(tokenRepository)
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .build()
+}
