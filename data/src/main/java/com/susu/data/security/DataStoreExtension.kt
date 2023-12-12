9648bf5fadd1086192a8b6c25c2f7667ab214af7
yangsooplus <yangsooplus@naver.com>
2023-12-12 15:19:06 +0900

    feat: 데이터에 암호화 저장, 접근 후 복호화하는 DataStore 확장함수 추가

---
+++ data/src/main/java/com/susu/data/security/DataStoreExtension.kt
@@ -0,0 +1,34 @@
+package com.susu.data.security
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.MutablePreferences
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+val json = Json { encodeDefaults = true }
+const val SEPARATOR = "|"
+
+suspend inline fun <reified T> DataStore<Preferences>.secureEdit(
+    value: T,
+    crossinline editStore: (MutablePreferences, String) -> Unit,
+) {
+    edit {
+        val encryptedValue = SecurityUtil.encrypt(Json.encodeToString(value))
+        editStore.invoke(it, encryptedValue.joinToString(SEPARATOR))
+    }
+}
+
+inline fun <reified T> Flow<Preferences>.secureMap(
+    crossinline fetchValue: (Preferences) -> String?,
+): Flow<T?> {
+    return map { pref ->
+        fetchValue(pref)?.let {
+            val decryptedValue = SecurityUtil.decrypt(it.split(SEPARATOR).map { it.toByte() }.toByteArray())
+            json.decodeFromString(decryptedValue)
+        }
+    }
+}
