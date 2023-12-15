95ac92713bd06523c7a0644a03f622ca129b218f
yangsooplus <yangsooplus@naver.com>
2023-12-15 17:29:24 +0900

    chore: Json 판별 시 Exception -> JsonException 이 터질때로 변경

--- data/src/main/java/com/susu/data/extension/StringExtension.kt
+++ data/src/main/java/com/susu/data/extension/StringExtension.kt
@@ -1,18 +1,19 @@
 package com.susu.data.extension
 
 import org.json.JSONArray
+import org.json.JSONException
 import org.json.JSONObject
 
 fun String.isJsonObject(): Boolean = try {
     JSONObject(this)
     true
-} catch (e: Exception) {
+} catch (e: JSONException) {
     false
 }
 
 fun String.isJsonArray(): Boolean = try {
     JSONArray(this)
     true
-} catch (e: Exception) {
+} catch (e: JSONException) {
     false
 }
