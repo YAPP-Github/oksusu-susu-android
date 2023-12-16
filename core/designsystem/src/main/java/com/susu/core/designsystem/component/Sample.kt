775f1b1a75d441fc1c4154068614e28c5cc9919c
jinukeu <koownij@kakao.com>
2023-12-16 16:29:05 +0900

    feat: Sample 파일 추가

---
+++ core/designsystem/src/main/java/com/susu/core/designsystem/component/Sample.kt
@@ -0,0 +1,17 @@
+package com.susu.core.designsystem.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.susu.core.designsystem.theme.SusuTheme
+
+@Preview
+@Composable
+fun Sample() {
+    SusuTheme {
+        Text(
+            text = "title_xxxxl",
+            style = SusuTheme.typography.title_xxxxl,
+        )
+    }
+}
