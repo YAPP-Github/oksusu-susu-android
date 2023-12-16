a63a2d8d71f3af288af408aeab730bb01160073a
yangsooplus <yangsooplus@naver.com>
2023-12-16 17:46:26 +0900

    chore: Json 판별 detekt 규칙에 맞게 수정

--- data/src/main/java/com/susu/data/extension/StringExtension.kt
+++ data/src/main/java/com/susu/data/extension/StringExtension.kt
@@ -1,19 +1,8 @@
 package com.susu.data.extension
 
 import org.json.JSONArray
-import org.json.JSONException
 import org.json.JSONObject
 
-fun String.isJsonObject(): Boolean = try {
-    JSONObject(this)
-    true
-} catch (e: JSONException) {
-    false
-}
+fun String.isJsonObject(): Boolean = runCatching { JSONObject(this) }.isSuccess
 
-fun String.isJsonArray(): Boolean = try {
-    JSONArray(this)
-    true
-} catch (e: JSONException) {
-    false
-}
+fun String.isJsonArray(): Boolean = runCatching { JSONArray(this) }.isSuccess
