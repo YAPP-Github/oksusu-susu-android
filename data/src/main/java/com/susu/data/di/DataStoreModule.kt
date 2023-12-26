7f09a876937b14b5c8264e16b8a16712dff6d2b5
yangsooplus <yangsooplus@naver.com>
2023-12-26 13:53:23 +0900

    refactor: encrpted-datastore로 교체

--- data/src/main/java/com/susu/data/di/DataStoreModule.kt
+++ data/src/main/java/com/susu/data/di/DataStoreModule.kt
@@ -7,11 +7,15 @@ import androidx.datastore.preferences.core.PreferenceDataStoreFactory
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStoreFile
+import com.susu.data.security.SecurityPreferences
+import com.susu.data.security.generateSecurityPreferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import tech.thdev.useful.encrypted.data.store.preferences.security.generateUsefulSecurity
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -29,5 +33,28 @@ object DataStoreModule {
         )
     }
 
+    @EncryptedDataStore
+    @Singleton
+    @Provides
+    fun providedEncryptedDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() },
+            ),
+            produceFile = { context.preferencesDataStoreFile(ENCRYPTED_DATASTORE_NAME) },
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideSecurityPreference(
+        @EncryptedDataStore dataStore: DataStore<Preferences>,
+    ): SecurityPreferences = dataStore.generateSecurityPreferences(generateUsefulSecurity())
+
     private const val DATASTORE_NAME = "susu-datastore"
+    private const val ENCRYPTED_DATASTORE_NAME = "susu-datastore-encrypted"
 }
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+internal annotation class EncryptedDataStore
--- domain/src/main/java/com/susu/domain/repository/TokenRepository.kt
+++ domain/src/main/java/com/susu/domain/repository/TokenRepository.kt
@@ -4,8 +4,8 @@ import com.susu.core.model.Token
 import kotlinx.coroutines.flow.Flow
 
 interface TokenRepository {
-    fun getAccessToken(): Flow<String?>
-    fun getRefreshToken(): Flow<String?>
+    fun getAccessToken(): Flow<String>
+    fun getRefreshToken(): Flow<String>
     suspend fun saveTokens(token: Token)
     suspend fun deleteTokens()
 }
