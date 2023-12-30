67ece75aef51ef296610a25a148906f89fd42088
syb8200 <syb8200@naver.com>
2023-12-31 00:53:08 +0900

    feat: SusuProgressAppBar Preview 수정 (애니메이션 확인용)

--- core/designsystem/src/main/java/com/susu/core/designsystem/component/appbar/SusuProgressAppBar.kt
+++ core/designsystem/src/main/java/com/susu/core/designsystem/component/appbar/SusuProgressAppBar.kt
@@ -1,11 +1,21 @@
 package com.susu.core.designsystem.component.appbar
 
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
@@ -56,6 +66,15 @@ fun SusuProgressBarAppBar(
 @Preview(showBackground = true)
 @Composable
 fun SusuProgressAppBarPreview() {
+    val currentPage = 6
+    val increasePercent = 1f / currentPage
+    var progress by remember { mutableStateOf(increasePercent) }
+
+    val progressAnimation by animateFloatAsState(
+        targetValue = progress,
+        animationSpec = tween(durationMillis = 1000, easing = FastOutSlowInEasing), label = ""
+    )
+
     SusuTheme {
         Column(
             verticalArrangement = Arrangement.spacedBy(20.dp),
@@ -63,8 +82,20 @@ fun SusuProgressAppBarPreview() {
             SusuProgressBarAppBar(
                 leftIconType = LeftIconType.BACKBUTTON,
                 leftIcon = R.drawable.ic_arrow_left,
-                currentPercentage = 0.5f
+                currentPercentage = progressAnimation
             )
+
+            Button(
+                onClick = {
+                    progress += increasePercent
+                }
+            ) {
+                Text(text = "다음")
+            }
+
+            LaunchedEffect(progress) {
+                progress = progress
+            }
         }
     }
 }
