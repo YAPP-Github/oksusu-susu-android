1dc78600182e6a78949b176b61745c22a83e10eb
yangsooplus <yangsooplus@naver.com>
2023-12-31 21:43:32 +0900

    feat: LoggingInterceptor level 설정 및 di

--- data/src/main/java/com/susu/data/di/NetworkModule.kt
+++ data/src/main/java/com/susu/data/di/NetworkModule.kt
@@ -29,32 +29,34 @@ object NetworkModule {
 
     @Singleton
     @Provides
+    fun provideLoggingInterceptor(
+        json: Json,
+    ): HttpLoggingInterceptor = HttpLoggingInterceptor { message ->
+        when {
+            !message.isJsonObject() && !message.isJsonArray() ->
+                Timber.tag(RETROFIT_TAG).d("CONNECTION INFO -> $message")
+
+            else -> kotlin.runCatching {
+                json.encodeToString(Json.parseToJsonElement(message))
+            }.onSuccess {
+                Timber.tag(RETROFIT_TAG).d(it)
+            }.onFailure {
+                Timber.tag(RETROFIT_TAG).d(message)
+            }
+        }
+    }.apply { level = HttpLoggingInterceptor.Level.BODY }
+
+    @Singleton
+    @Provides
     fun provideOkHttpClient(
         tokenInterceptor: TokenInterceptor,
         tokenAuthenticator: TokenAuthenticator,
-        json: Json,
-    ): OkHttpClient {
-        val loggingInterceptor = HttpLoggingInterceptor { message ->
-            when {
-                !message.isJsonObject() && !message.isJsonArray() ->
-                    Timber.tag(RETROFIT_TAG).d("CONNECTION INFO -> $message")
-
-                else -> kotlin.runCatching {
-                    json.encodeToString(Json.parseToJsonElement(message))
-                }.onSuccess {
-                    Timber.tag(RETROFIT_TAG).d(it)
-                }.onFailure {
-                    Timber.tag(RETROFIT_TAG).d(message)
-                }
-            }
-        }
-
-        return OkHttpClient.Builder()
-            .addInterceptor(tokenInterceptor)
-            .addInterceptor(loggingInterceptor)
-            .authenticator(tokenAuthenticator)
-            .build()
-    }
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(tokenInterceptor)
+        .addInterceptor(loggingInterceptor)
+        .authenticator(tokenAuthenticator)
+        .build()
 
     @Singleton
     @Provides
@@ -71,27 +73,11 @@ object NetworkModule {
     @Provides
     @AuthOkHttpClient
     fun provideAuthOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
         json: Json,
-    ): OkHttpClient {
-        val loggingInterceptor = HttpLoggingInterceptor { message ->
-            when {
-                !message.isJsonObject() && !message.isJsonArray() ->
-                    Timber.tag(RETROFIT_TAG).d("CONNECTION INFO -> $message")
-
-                else -> kotlin.runCatching {
-                    json.encodeToString(Json.parseToJsonElement(message))
-                }.onSuccess {
-                    Timber.tag(RETROFIT_TAG).d(it)
-                }.onFailure {
-                    Timber.tag(RETROFIT_TAG).d(message)
-                }
-            }
-        }
-
-        return OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .build()
-    }
+    ): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
 
     @Singleton
     @Provides
