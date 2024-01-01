dce5048be7c9c09963dd76f47a7bef9b26866a5c
syb8200 <syb8200@naver.com>
2024-01-01 18:28:25 +0900

    refactor: SusuProgressAppBar 수정

--- core/designsystem/src/main/java/com/susu/core/designsystem/component/appbar/SusuProgressAppBar.kt
+++ core/designsystem/src/main/java/com/susu/core/designsystem/component/appbar/SusuProgressAppBar.kt
@@ -1,102 +1,103 @@
 package com.susu.core.designsystem.component.appbar
 
+import android.util.Log
 import androidx.annotation.DrawableRes
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.susu.core.designsystem.R
-import com.susu.core.designsystem.theme.Orange30
 import com.susu.core.designsystem.theme.SusuTheme
+import com.susu.core.ui.extension.susuClickable
 
 @Composable
-fun SusuProgressBarAppBar(
+fun SusuProgressAppBar(
     modifier: Modifier = Modifier,
-    containerHeight: Dp = 44.dp,
-    leftIconType: LeftIconType,
     @DrawableRes leftIcon: Int,
-    currentPercentage: Float,
-    progressBarColor: Color = SusuTheme.colorScheme.primary,
-    progressTrackColor: Color = Orange30,
-    progressBarWidth: Dp = 72.dp, // TODO: 디자인시스템과 최종시안이 다름, 현재 확인 중 (지울 예정)
-    progressBarHeight: Dp = 4.dp,
-    progressBarStrokeCap: StrokeCap = StrokeCap.Round,
-    onClickBackButton: () -> Unit = {},
+    leftIconContentDescription: String,
+    leftIconPadding: Dp = SusuTheme.spacing.spacing_xs,
+    currentStep: Int,
+    entireStep: Int,
+    progressBar: ProgressBarStyle = ProgressBarStyle.SusuProgressBar,
+    onClickBackButton: () -> Unit,
 ) {
     BasicAppBar(
-        containerHeight = containerHeight,
-        leftIconType = leftIconType,
-        leftIcon = leftIcon,
+        modifier = modifier,
+        leftIcon = {
+            Box(
+                modifier = modifier
+                    .susuClickable(
+                        rippleEnabled = false,
+                        onClick = onClickBackButton,
+                    )
+                    .padding(leftIconPadding),
+                contentAlignment = Alignment.Center,
+            ) {
+                Icon(
+                    painter = painterResource(id = leftIcon),
+                    contentDescription = leftIconContentDescription,
+                )
+            }
+        },
         title = {
             LinearProgressIndicator(
-                progress = {
-                    currentPercentage
-                },
-                color = progressBarColor,
-                trackColor = progressTrackColor,
+                progress = { currentStep / entireStep.toFloat() },
+                color = progressBar.progressBarColor,
+                trackColor = progressBar.progressBarTrackColor,
                 modifier = modifier
                     .size(
-                        width = progressBarWidth,
-                        height = progressBarHeight,
+                        width = progressBar.progressBarWidth,
+                        height = progressBar.progressBarHeight,
                     ),
-                strokeCap = progressBarStrokeCap,
+                strokeCap = progressBar.progressBarStrokeCap,
             )
         },
-        onClickBackButton = { onClickBackButton },
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
 @Composable
-fun SusuProgressAppBarPreview() {
-    // TODO: 애니메이션 부분 + 버튼은 단순히 확인 용으로 추가함 (지울 예정)
-    val currentPage = 6
-    val increasePercent = 1f / currentPage
-    var progress by remember { mutableStateOf(increasePercent) }
-
-    val progressAnimation by animateFloatAsState(
-        targetValue = progress,
-        animationSpec = tween(durationMillis = 1000, easing = FastOutSlowInEasing),
-        label = "",
-    )
+fun SusuProgressAppBarPreview(
+    modifier: Modifier = Modifier,
+) {
+    val entireStep = 6
+    var currentStep by remember { mutableStateOf(1) }
 
     SusuTheme {
         Column(
             verticalArrangement = Arrangement.spacedBy(20.dp),
         ) {
-            SusuProgressBarAppBar(
-                leftIconType = LeftIconType.BACKBUTTON,
+            SusuProgressAppBar(
                 leftIcon = R.drawable.ic_arrow_left,
-                currentPercentage = progressAnimation,
+                leftIconContentDescription = "뒤로가기",
+                currentStep = currentStep,
+                entireStep = entireStep,
+                onClickBackButton = {
+                    Log.d("확인", "왼쪽 뒤로가기 클릭")
+                },
             )
-
             Button(
                 onClick = {
-                    progress += increasePercent
+                    currentStep += 1
                 },
             ) {
                 Text(text = "다음")
-            }
-
-            LaunchedEffect(progress) {
-                progress = progress
             }
         }
     }
