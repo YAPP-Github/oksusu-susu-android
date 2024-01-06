17284c9b820cf3ad80caaf5503c84c297ae4cdac
yangsooplus <yangsooplus@naver.com>
2024-01-06 15:05:26 +0900

    refactor: Navigation을 XXXRoute로 변경

--- feature/loginsignup/src/main/java/com/susu/feature/loginsignup/navigation/LoginSignupNavigation.kt
+++ feature/loginsignup/src/main/java/com/susu/feature/loginsignup/navigation/LoginSignupNavigation.kt
@@ -4,8 +4,8 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.susu.feature.loginsignup.VoteScreen
-import com.susu.feature.loginsignup.login.LoginScreen
+import com.susu.feature.loginsignup.VoteRoute
+import com.susu.feature.loginsignup.login.LoginRoute
 import com.susu.feature.loginsignup.signup.SignUpScreen
 
 @Suppress("unused")
@@ -18,12 +18,12 @@ fun NavGraphBuilder.loginSignupNavGraph(
     navigateToReceived: () -> Unit,
 ) {
     composable(route = LoginSignupRoute.Parent.Vote.route) {
-        VoteScreen(
+        VoteRoute(
             navigateToLogin = { navController.navigate(LoginSignupRoute.Parent.Login.route) },
         )
     }
     composable(route = LoginSignupRoute.Parent.Login.route) {
-        LoginScreen(
+        LoginRoute(
             navigateToReceived = navigateToReceived,
             navigateToSignUp = { navController.navigate(LoginSignupRoute.Parent.SignUp.route) },
         )
