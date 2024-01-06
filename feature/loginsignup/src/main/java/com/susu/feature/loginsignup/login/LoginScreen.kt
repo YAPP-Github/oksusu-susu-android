07a9551aec3d70bcec0ef2788d2601c31dd7f8f1
yangsooplus <yangsooplus@naver.com>
2024-01-06 17:56:12 +0900

    fix: collectAsState -> collectAsStateWithLifecycle

--- feature/loginsignup/src/main/java/com/susu/feature/loginsignup/login/LoginScreen.kt
+++ feature/loginsignup/src/main/java/com/susu/feature/loginsignup/login/LoginScreen.kt
@@ -22,7 +22,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -35,6 +34,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.susu.core.designsystem.component.util.LoadingScreen
 import com.susu.core.designsystem.theme.Gray10
 import com.susu.core.designsystem.theme.Gray50
@@ -52,7 +52,7 @@ fun LoginRoute(
 ) {
     val context = LocalContext.current
     // TODO: Loading 처리
-    val uiState by viewModel.uiState.collectAsState()
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val transitionState = remember {
         MutableTransitionState(false).apply {
             targetState = true
