4ad0d5af12095107f7bf6db6cec039ed9a68d199
yangsooplus <yangsooplus@naver.com>
2024-01-10 18:47:08 +0900

    feat: SusuDatePickerBottomSheet 컴포넌트 추가

--- core/designsystem/src/main/java/com/susu/core/designsystem/component/bottomsheet/datepicker/SusuDatePickerBottomSheet.kt
+++ core/designsystem/src/main/java/com/susu/core/designsystem/component/bottomsheet/datepicker/SusuDatePickerBottomSheet.kt
@@ -1,6 +1,8 @@
 package com.susu.core.designsystem.component.bottomsheet.datepicker
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -9,14 +11,87 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.susu.core.designsystem.component.bottomsheet.SusuBottomSheet
 import com.susu.core.designsystem.theme.SusuTheme
-import okhttp3.internal.toImmutableList
+import kotlinx.collections.immutable.toImmutableList
 import java.time.LocalDate
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SusuDatePickerBottomSheet(
+    sheetState: SheetState = rememberModalBottomSheetState(),
+    yearRange: IntRange = 1930..2030,
+    maximumContainerHeight: Dp,
+    itemHeight: Dp = 48.dp,
+    numberOfDisplayedItems: Int = 5,
+    cornerRadius: Dp = 24.dp,
+    onDismissRequest: (Int, Int, Int) -> Unit = { _, _, _ -> },
+) {
+    var selectedYear by remember { mutableIntStateOf(LocalDate.now().year) }
+    var selectedMonth by remember { mutableIntStateOf(LocalDate.now().monthValue) }
+    var selectedDay by remember { mutableIntStateOf(LocalDate.now().dayOfMonth) }
+    var lastDayInMonth by remember { mutableIntStateOf(getLastDayInMonth(LocalDate.now().year, LocalDate.now().monthValue)) }
+
+    val yearList = yearRange.map { "${it}년" }.toImmutableList()
+    val monthList = (1..12).map { "${it}월" }.toImmutableList()
+
+    SusuBottomSheet(
+        sheetState = sheetState,
+        containerHeight = minOf(maximumContainerHeight, itemHeight * numberOfDisplayedItems + 32.dp),
+        onDismissRequest = { onDismissRequest(selectedYear, selectedMonth, selectedDay) },
+        cornerRadius = cornerRadius,
+    ) {
+        Row {
+            InfiniteColumn(
+                modifier = Modifier.width(100.dp),
+                items = yearList,
+                initialItem = "${LocalDate.now().year}년",
+                itemHeight = itemHeight,
+                numberOfDisplayedItems = numberOfDisplayedItems,
+                onItemSelected = { _, item ->
+                    selectedYear = item.dropLast(1).toIntOrNull() ?: LocalDate.now().year
+                    getLastDayInMonth(selectedYear, selectedMonth).run {
+                        lastDayInMonth = this
+                        if (selectedDay > lastDayInMonth) {
+                            selectedDay = 1
+                        }
+                    }
+                },
+            )
+            InfiniteColumn(
+                modifier = Modifier.width(100.dp),
+                items = monthList,
+                initialItem = "${LocalDate.now().monthValue}월",
+                itemHeight = itemHeight,
+                numberOfDisplayedItems = numberOfDisplayedItems,
+                onItemSelected = { _, item ->
+                    selectedMonth = item.dropLast(1).toIntOrNull() ?: LocalDate.now().monthValue
+                    getLastDayInMonth(selectedYear, selectedMonth).run {
+                        lastDayInMonth = this
+                        if (selectedDay > lastDayInMonth) {
+                            selectedDay = 1
+                        }
+                    }
+                },
+            )
+            InfiniteColumn(
+                modifier = Modifier.width(100.dp),
+                items = (1..lastDayInMonth).map { "${it}일" }.toMutableList(),
+                initialItem = "${selectedDay}일",
+                itemHeight = itemHeight,
+                numberOfDisplayedItems = numberOfDisplayedItems,
+                onItemSelected = { _, item ->
+                    selectedDay = item.dropLast(1).toIntOrNull() ?: 1
+                },
+            )
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,7 +114,7 @@ fun SusuYearPickerBottomSheet(
         cornerRadius = cornerRadius,
     ) {
         InfiniteColumn(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally),
             items = yearList,
             initialItem = "${initialYear}년",
             itemHeight = itemHeight,
@@ -51,11 +126,27 @@ fun SusuYearPickerBottomSheet(
     }
 }
 
+private fun getLastDayInMonth(year: Int, month: Int): Int {
+    val isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)
+
+    return when (month) {
+        1, 3, 5, 7, 8, 10, 12 -> 31
+        4, 6, 9, 11 -> 30
+        2 -> if (isLeapYear) 29 else 28
+        else -> throw IllegalArgumentException("Invalid month: $month")
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun SusuYearPickerBottomSheetPreview() {
+fun SusuDatePickerBottomSheetPreview() {
     SusuTheme {
-        SusuYearPickerBottomSheet(maximumContainerHeight = 346.dp, onDismissRequest = { println(it) })
+        SusuDatePickerBottomSheet(
+            maximumContainerHeight = 346.dp,
+            onDismissRequest = { y, m, d ->
+                println("$y $m $d")
+            },
+        )
     }
 }
