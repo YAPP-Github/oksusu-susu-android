767d9f7273ce2a31e80ab496500cc555bd80b0da
jinukeu <koownij@kakao.com>
2024-01-10 10:27:06 +0900

    feat: Filter Icon size 조정

--- feature/received/src/main/java/com/susu/feature/received/ledgerdetail/LedgerDetailScreen.kt
+++ feature/received/src/main/java/com/susu/feature/received/ledgerdetail/LedgerDetailScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -120,6 +121,7 @@ fun LedgerDetailScreen(
                             text = stringResource(R.string.word_filter),
                             leftIcon = {
                                 Icon(
+                                    modifier = Modifier.size(16.dp),
                                     painter = painterResource(id = R.drawable.ic_filter),
                                     contentDescription = null,
                                 )
