a9559e693a643adf0b9194f1cbc5cbbd75b82502
jinukeu <koownij@kakao.com>
2024-01-12 20:28:53 +0900

    feat: Date Util 추가

---
+++ core/ui/src/main/java/com/susu/core/ui/util/Date.kt
@@ -0,0 +1,11 @@
+package com.susu.core.ui.util
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+val currentDate = LocalDateTime.now()
+
+fun LocalDateTime.to_yyyy_dot_MM_dot_dd(): String {
+    val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
+    return this.format(formatter)
+}
