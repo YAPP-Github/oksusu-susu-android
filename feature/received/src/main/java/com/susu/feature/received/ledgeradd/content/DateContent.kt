f2d88bd888d8dab0671fdcc0506cb4878b0d04fd
jinukeu <koownij@kakao.com>
2024-01-12 18:08:41 +0900

    feat: 날짜 선택 ui 구현

--- feature/received/src/main/java/com/susu/feature/received/ledgeradd/content/DateContent.kt
+++ feature/received/src/main/java/com/susu/feature/received/ledgeradd/content/DateContent.kt
@@ -5,17 +5,19 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.susu.core.designsystem.theme.Gray80
+import androidx.compose.ui.unit.dp
+import com.susu.core.designsystem.component.bottomsheet.datepicker.SusuDatePickerBottomSheet
 import com.susu.core.designsystem.theme.SusuTheme
-import com.susu.core.ui.util.AnnotatedText
 import com.susu.feature.received.R
+import com.susu.feature.received.ledgeradd.content.component.SelectDateRow
 
-// TODO 디자인 변경 예정
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DateContent() {
     Column(
@@ -27,34 +29,26 @@ fun DateContent() {
                 end = SusuTheme.spacing.spacing_m,
             ),
     ) {
+        // TODO Annotated Text 사용
         Text(
-            text = "경조사 기간을 입력해주세요",
+            text = "고모부의 장례식은 언제인가요",
             style = SusuTheme.typography.title_m,
         )
 
         Spacer(modifier = Modifier.size(SusuTheme.spacing.spacing_m))
 
-        AnnotatedText(
-            originalText = stringResource(R.string.ledger_edit_screen_from_date, 2023, 11, 25),
-            targetTextList = listOf(
-                stringResource(R.string.ledger_edit_screen_year),
-                stringResource(R.string.ledger_edit_screen_month),
-                stringResource(R.string.ledger_edit_screen_from_day),
-            ),
-            originalTextStyle = SusuTheme.typography.title_xl,
-            spanStyle = SusuTheme.typography.title_xl.copy(Gray80).toSpanStyle(),
+        SelectDateRow(
+            suffix = stringResource(R.string.ledger_add_screen_from),
         )
-        AnnotatedText(
-            originalText = stringResource(R.string.ledger_edit_screen_until_date, 2023, 11, 25),
-            targetTextList = listOf(
-                stringResource(R.string.ledger_edit_screen_year),
-                stringResource(R.string.ledger_edit_screen_month),
-                stringResource(R.string.ledger_edit_screen_until_day),
-            ),
-            originalTextStyle = SusuTheme.typography.title_xl,
-            spanStyle = SusuTheme.typography.title_xl.copy(Gray80).toSpanStyle(),
+
+        Spacer(modifier = Modifier.size(SusuTheme.spacing.spacing_xxs))
+
+        SelectDateRow(
+            suffix = stringResource(R.string.ledger_add_screen_until),
         )
     }
+
+    SusuDatePickerBottomSheet(maximumContainerHeight = 346.dp)
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFF6F6F6)
