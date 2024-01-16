07d0c1935019bf062d95fbf89076314730575298
yangsooplus <yangsooplus@naver.com>
2024-01-16 20:24:21 +0900

    chore: unused import 제거

--- data/src/main/java/com/susu/data/data/repository/LoginRepositoryImpl.kt
+++ data/src/main/java/com/susu/data/data/repository/LoginRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.susu.data.data.repository
 
 import com.susu.data.remote.api.UserService
-import com.susu.data.remote.model.SnsProviders
 import com.susu.data.remote.model.request.AccessTokenRequest
 import com.susu.data.remote.model.response.toDomain
 import com.susu.domain.repository.LoginRepository
