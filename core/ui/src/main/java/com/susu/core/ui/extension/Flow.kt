bc0ad850765904c6aef3ac4d05e601db59ff69db
jinukeu <koownij@kakao.com>
2024-01-17 22:11:04 +0900

    fix: collectWithLifecycle 확장 함수의 key가 올바르게 설정되어있지 않던 문제 해결

--- core/ui/src/main/java/com/susu/core/ui/extension/Flow.kt
+++ core/ui/src/main/java/com/susu/core/ui/extension/Flow.kt
@@ -7,20 +7,21 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 @Composable
 inline fun <reified T> Flow<T>.collectWithLifecycle(
-    lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current,
     minActiveState: Lifecycle.State = Lifecycle.State.STARTED,
     noinline action: suspend (T) -> Unit,
 ) {
-    LaunchedEffect(key1 = Unit) {
-        lifecycleOwner.lifecycleScope.launch {
-            flowWithLifecycle(lifecycleOwner.lifecycle, minActiveState).collect {
-                action(it)
-            }
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(this, lifecycleOwner) {
+        lifecycleOwner.lifecycle.repeatOnLifecycle(minActiveState) {
+            this@collectWithLifecycle.collect { action(it) }
         }
     }
 }
