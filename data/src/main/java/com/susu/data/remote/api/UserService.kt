4f231ad097b0cc4fe42d5ce9ddb8f338da03c9e5
yangsooplus <yangsooplus@naver.com>
2024-01-18 17:18:03 +0900

    fix: patchUserInfo @Body 어노테이션 누락 수정

--- data/src/main/java/com/susu/data/remote/api/UserService.kt
+++ data/src/main/java/com/susu/data/remote/api/UserService.kt
@@ -3,6 +3,7 @@ package com.susu.data.remote.api
 import com.susu.data.remote.model.request.UserPatchRequest
 import com.susu.data.remote.model.response.UserResponse
 import com.susu.data.remote.retrofit.ApiResult
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.Path
@@ -12,5 +13,5 @@ interface UserService {
     suspend fun getUserInfo(): ApiResult<UserResponse>
 
     @PATCH("users/{uid}")
-    suspend fun patchUserInfo(@Path("uid") uid: Int, userPatchRequest: UserPatchRequest): ApiResult<UserResponse>
+    suspend fun patchUserInfo(@Path("uid") uid: Int, @Body userPatchRequest: UserPatchRequest): ApiResult<UserResponse>
 }
