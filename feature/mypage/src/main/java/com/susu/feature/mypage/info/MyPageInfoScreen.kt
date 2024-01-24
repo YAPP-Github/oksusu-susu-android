09dfcb9b2e370dd2916ead4852de8e48b2d0150e
yangsooplus <yangsooplus@naver.com>
2024-01-24 22:34:02 +0900

    feat: 내정보 편집 이름 형식 불만족시 붉은 점 표시

--- feature/mypage/src/main/java/com/susu/feature/mypage/info/MyPageInfoScreen.kt
+++ feature/mypage/src/main/java/com/susu/feature/mypage/info/MyPageInfoScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -157,6 +156,7 @@ fun MyPageInfoScreen(
             Spacer(modifier = Modifier.height(SusuTheme.spacing.spacing_m))
             MyPageInfoItem(
                 title = stringResource(id = com.susu.core.ui.R.string.word_name),
+                isWrong = uiState.isEditing && !uiState.isEditNameValid,
             ) {
                 if (uiState.isEditing) {
                     SusuBasicTextField(
