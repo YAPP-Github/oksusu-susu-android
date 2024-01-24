4ef4f3b0245c453f3c06c6e5d45fb07321644fd4
jinukeu <koownij@kakao.com>
2024-01-24 16:10:06 +0900

    feat: TextFieldButtonColor Gray 추가

--- core/designsystem/src/main/java/com/susu/core/designsystem/component/button/ClearIconButton.kt
+++ core/designsystem/src/main/java/com/susu/core/designsystem/component/button/ClearIconButton.kt
@@ -3,25 +3,30 @@ package com.susu.core.designsystem.component.button
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import com.susu.core.designsystem.R
+import com.susu.core.designsystem.theme.Gray30
 import com.susu.core.ui.extension.susuClickable
 
 @Composable
 fun ClearIconButton(
     iconSize: Dp,
+    tint: Color = Gray30,
     onClick: () -> Unit,
 ) {
-    Image(
+    Icon(
         modifier = Modifier
             .clip(CircleShape)
             .size(iconSize)
             .susuClickable(onClick = onClick),
         painter = painterResource(id = R.drawable.ic_clear),
         contentDescription = "",
+        tint = tint,
     )
 }
