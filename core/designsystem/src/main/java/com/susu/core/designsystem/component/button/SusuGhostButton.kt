293a417a8969d88584267f846f34edf22e3e0065
jinukeu <koownij@kakao.com>
2024-01-25 15:57:24 +0900

    feat: SusuButton - TextModifier, TextAlign 속성 추가

--- core/designsystem/src/main/java/com/susu/core/designsystem/component/button/SusuGhostButton.kt
+++ core/designsystem/src/main/java/com/susu/core/designsystem/component/button/SusuGhostButton.kt
@@ -12,6 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.susu.core.designsystem.R
@@ -49,6 +50,8 @@ enum class GhostButtonColor(
 fun SusuGhostButton(
     modifier: Modifier = Modifier,
     shape: Shape = RoundedCornerShape(4.dp),
+    textModifier: Modifier = Modifier,
+    textAlign: TextAlign = TextAlign.Unspecified,
     text: String? = null,
     color: GhostButtonColor,
     style: @Composable () -> ButtonStyle,
@@ -64,8 +67,9 @@ fun SusuGhostButton(
         BasicButton(
             modifier = modifier,
             shape = shape,
+            textModifier = textModifier,
             text = text,
-            textStyle = textStyle,
+            textStyle = textStyle.copy(textAlign = textAlign),
             contentColor = if (isActive) activeContentColor else inactiveContentColor,
             rippleColor = rippleColor,
             backgroundColor = if (isActive) activeBackgroundColor else inactiveBackgroundColor,
--- core/designsystem/src/main/java/com/susu/core/designsystem/component/button/SusuLinedButton.kt
+++ core/designsystem/src/main/java/com/susu/core/designsystem/component/button/SusuLinedButton.kt
@@ -8,10 +8,12 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.susu.core.designsystem.R
@@ -56,6 +58,8 @@ enum class LinedButtonColor(
 fun SusuLinedButton(
     modifier: Modifier = Modifier,
     shape: Shape = RoundedCornerShape(4.dp),
+    textModifier: Modifier = Modifier,
+    textAlign: TextAlign = TextAlign.Unspecified,
     text: String? = null,
     color: LinedButtonColor,
     style: @Composable () -> ButtonStyle,
@@ -70,8 +74,9 @@ fun SusuLinedButton(
         BasicButton(
             modifier = modifier,
             shape = shape,
+            textModifier = textModifier,
             text = text,
-            textStyle = textStyle,
+            textStyle = textStyle.copy(textAlign = textAlign),
             borderWidth = 1.dp,
             borderColor = if (isActive) activeBorderColor else inactiveBorderColor,
             contentColor = if (isActive) activeContentColor else inactiveContentColor,
