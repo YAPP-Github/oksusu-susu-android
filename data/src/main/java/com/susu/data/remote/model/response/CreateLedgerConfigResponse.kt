b9517124640aa1450834fb5c497e9a2d2fef2770
jinukeu <koownij@kakao.com>
2024-01-26 15:10:12 +0900

    feat: 장부 추가 시, 카테고리에 따라 시작일만 보이게 설정

--- data/src/main/java/com/susu/data/remote/model/response/CreateLedgerConfigResponse.kt
+++ data/src/main/java/com/susu/data/remote/model/response/CreateLedgerConfigResponse.kt
@@ -1,5 +1,8 @@
 package com.susu.data.remote.model.response
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CreateLedgerConfigResponse(
     val onlyStartAtCategoryIds: List<Int>,
 )
--- feature/received/src/main/java/com/susu/feature/received/ledgeradd/content/date/DateViewModel.kt
+++ feature/received/src/main/java/com/susu/feature/received/ledgeradd/content/date/DateViewModel.kt
@@ -1,15 +1,33 @@
 package com.susu.feature.received.ledgeradd.content.date
 
+import androidx.lifecycle.viewModelScope
+import com.susu.core.model.Category
 import com.susu.core.ui.base.BaseViewModel
 import com.susu.core.ui.util.getSafeLocalDateTime
+import com.susu.domain.usecase.ledger.GetCreateLedgerConfigUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
-class DateViewModel @Inject constructor() : BaseViewModel<DateState, DateSideEffect>(
+class DateViewModel @Inject constructor(
+    private val getCreateLedgerConfigUseCase: GetCreateLedgerConfigUseCase,
+) : BaseViewModel<DateState, DateSideEffect>(
     DateState(),
 ) {
+    private var category = Category()
+
+    fun setScreenType(category: Category) = viewModelScope.launch {
+        if (this@DateViewModel.category == category) return@launch
+
+        getCreateLedgerConfigUseCase()
+            .onSuccess { onlyStartAtCategoryIdList ->
+                intent { copy(showOnlyStartAt = category.id in onlyStartAtCategoryIdList) }
+            }
+    }
+
     fun updateNameAndCategory(name: String, categoryName: String) = intent {
         copy(
             name = name,
@@ -19,9 +37,11 @@ class DateViewModel @Inject constructor() : BaseViewModel<DateState, DateSideEff
 
     fun updateStartDate(year: Int, month: Int, day: Int) = intent {
         val toUpdateStartAt = LocalDateTime.of(year, month, day, 0, 0)
-        postSideEffect(DateSideEffect.UpdateParentDate(toUpdateStartAt, endAt))
+        val toUpdateEndAt = if (showOnlyStartAt) toUpdateStartAt else endAt
+        postSideEffect(DateSideEffect.UpdateParentDate(toUpdateStartAt, toUpdateEndAt))
         copy(
             startAt = toUpdateStartAt,
+            endAt = toUpdateEndAt,
         )
     }
 
@@ -33,12 +53,23 @@ class DateViewModel @Inject constructor() : BaseViewModel<DateState, DateSideEff
         )
     }
 
-    fun updateParentDate() = with(currentState) {
-        postSideEffect(DateSideEffect.UpdateParentDate(startAt, endAt))
-    }
-
     fun showStartDateBottomSheet() = intent { copy(showStartDateBottomSheet = true) }
     fun hideStartDateBottomSheet() = intent { copy(showStartDateBottomSheet = false) }
     fun showEndDateBottomSheet() = intent { copy(showEndDateBottomSheet = true) }
     fun hideEndDateBottomSheet() = intent { copy(showEndDateBottomSheet = false) }
+    fun toggleShowOnlyStartAt() = intent {
+        if (showOnlyStartAt) {
+            postSideEffect(DateSideEffect.UpdateParentDate(startAt, null))
+            copy(
+                showOnlyStartAt = false,
+                endAt = null
+            )
+        } else {
+            postSideEffect(DateSideEffect.UpdateParentDate(startAt, startAt))
+            copy(
+                showOnlyStartAt = true,
+                endAt = startAt
+            )
+        }
+    }
 }
