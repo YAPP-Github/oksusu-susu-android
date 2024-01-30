71a33416f58eacb3b4245851323598c0a09c4a31
yangsooplus <yangsooplus@naver.com>
2024-01-30 15:30:19 +0900

    feat: SusuCheckedDialog 컴포넌트 추가

--- core/ui/src/main/java/com/susu/core/ui/DialogToken.kt
+++ core/ui/src/main/java/com/susu/core/ui/DialogToken.kt
@@ -2,13 +2,23 @@ package com.susu.core.ui
 
 import androidx.compose.ui.text.style.TextAlign
 
+/**
+ * checkboxText: 체크박스 메세지. null 일 경우 SusuDialog, null이 아닐 경우 SusuCheckedDialog 가 노출
+ *
+ * defaultChecked: Dialog 노출 시 기본 체크 상태
+ *
+ * onCheckedAction: 체크박스 선택 시 추가로 실행하는 로직
+ * */
 data class DialogToken(
     val title: String? = null,
     val text: String? = null,
     val confirmText: String = "",
     val dismissText: String? = null,
+    val checkboxText: String? = null,
+    val defaultChecked: Boolean = false,
     val isDimmed: Boolean = true,
     val textAlign: TextAlign = TextAlign.Center,
     val onConfirmRequest: () -> Unit = {},
+    val onCheckedAction: () -> Unit = {},
     val onDismissRequest: () -> Unit = {},
 )
