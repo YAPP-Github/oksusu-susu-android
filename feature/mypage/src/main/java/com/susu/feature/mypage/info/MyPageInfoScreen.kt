a70ca96db1bfb569a4e4a4b0eebfd685ac4748ac
yangsooplus <yangsooplus@naver.com>
2024-02-10 20:36:51 +0900

    fix: 내 정보 출생년도 누락된 '년' 추가

--- feature/mypage/src/main/java/com/susu/feature/mypage/info/MyPageInfoScreen.kt
+++ feature/mypage/src/main/java/com/susu/feature/mypage/info/MyPageInfoScreen.kt
@@ -194,7 +194,7 @@ fun MyPageInfoScreen(
                             onClick = onBirthClick,
                         ),
                         text = if (uiState.editBirth in USER_BIRTH_RANGE) {
-                            uiState.editBirth.toString()
+                            stringResource(id = com.susu.core.designsystem.R.string.word_year_format, uiState.editBirth)
                         } else {
                             stringResource(id = com.susu.core.ui.R.string.word_not_select)
                         },
@@ -204,7 +204,7 @@ fun MyPageInfoScreen(
                 } else {
                     Text(
                         text = if (uiState.userBirth in USER_BIRTH_RANGE) {
-                            uiState.userBirth.toString()
+                            stringResource(id = com.susu.core.designsystem.R.string.word_year_format, uiState.userBirth)
                         } else {
                             stringResource(id = com.susu.core.ui.R.string.word_not_select)
                         },
@@ -263,6 +263,7 @@ fun MyPageInfoScreen(
             SusuYearPickerBottomSheet(
                 maximumContainerHeight = 322.dp,
                 yearRange = USER_BIRTH_RANGE,
+                reverseItemOrder = true,
                 onDismissRequest = { onBirthSelect(it) },
             )
         }
