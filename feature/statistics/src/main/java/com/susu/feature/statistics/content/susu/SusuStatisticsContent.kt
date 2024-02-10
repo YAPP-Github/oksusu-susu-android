5f5a5198beee2c1f991bf5b3b2f8a36cdc199157
yangsooplus <yangsooplus@naver.com>
2024-02-10 21:17:12 +0900

    fix/QA-29: 통계 페이지 하단 여백 추가

--- feature/statistics/src/main/java/com/susu/feature/statistics/content/susu/SusuStatisticsContent.kt
+++ feature/statistics/src/main/java/com/susu/feature/statistics/content/susu/SusuStatisticsContent.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
@@ -184,6 +185,7 @@ fun SusuStatisticsScreen(
                     isActive = !isBlind,
                 )
             }
+            Spacer(modifier = Modifier.height(SusuTheme.spacing.spacing_xxxl))
         }
 
         if (uiState.isAgeSheetOpen) {
