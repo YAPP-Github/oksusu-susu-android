300f64ad8c5817fe0252faa6cee65db59e490360
jinukeu <koownij@kakao.com>
2024-02-16 09:20:27 +0900

    feat: 장부 생성시, 키보드가 자동으로 올라오도록 설정

--- feature/received/src/main/java/com/susu/feature/received/ledgeradd/content/name/NameContract.kt
+++ feature/received/src/main/java/com/susu/feature/received/ledgeradd/content/name/NameContract.kt
@@ -2,6 +2,7 @@ package com.susu.feature.received.ledgeradd.content.name
 
 import com.susu.core.ui.base.SideEffect
 import com.susu.core.ui.base.UiState
+import com.susu.feature.received.envelopeadd.content.present.PresentSideEffect
 
 data class NameState(
     val name: String = "",
@@ -9,4 +10,5 @@ data class NameState(
 
 sealed interface NameSideEffect : SideEffect {
     data class UpdateParentName(val name: String) : NameSideEffect
+    data object ShowKeyboard: NameSideEffect
 }
--- feature/received/src/main/java/com/susu/feature/received/ledgeradd/content/name/NameViewModel.kt
+++ feature/received/src/main/java/com/susu/feature/received/ledgeradd/content/name/NameViewModel.kt
@@ -1,7 +1,11 @@
 package com.susu.feature.received.ledgeradd.content.name
 
+import androidx.lifecycle.viewModelScope
 import com.susu.core.ui.base.BaseViewModel
+import com.susu.feature.received.envelopeadd.content.present.PresentSideEffect
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,5 +15,12 @@ class NameViewModel @Inject constructor() : BaseViewModel<NameState, NameSideEff
     fun updateName(name: String) = intent {
         postSideEffect(NameSideEffect.UpdateParentName(name))
         copy(name = name)
+    }
+
+    fun showKeyboardIfTextEmpty() = viewModelScope.launch {
+        if (currentState.name.isEmpty()) {
+            delay(400L)
+            postSideEffect(NameSideEffect.ShowKeyboard)
+        }
     }
 }
