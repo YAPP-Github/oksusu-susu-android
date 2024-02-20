321d06930a30fc9b1f06915ef862ba7cff3be3e6
jinukeu <koownij@kakao.com>
2024-02-20 23:36:39 +0900

    fix/QA-98: 보내요 탭 → 인물 봉투 → 봉투 상세 진입 시 앱 꺼짐 발생

--- core/ui/src/main/java/com/susu/core/ui/extension/String.kt
+++ core/ui/src/main/java/com/susu/core/ui/extension/String.kt
@@ -1,5 +1,5 @@
 package com.susu.core.ui.extension
 
-fun String.toPhoneNumber(): String {
-    return "${this.substring(0, 3)}-${this.substring(3, 7)}-${this.substring(7)}"
-}
+fun String.toPhoneNumber(): String = runCatching {
+    "${this.substring(0, 3)}-${this.substring(3, 7)}-${this.substring(7)}"
+}.getOrNull() ?: this
